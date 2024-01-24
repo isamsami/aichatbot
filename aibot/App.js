ae1abcb4113364b9cdeeba18857aecaa567295ec
isamsami <samisalah.am@gmail.com>
2024-01-23 22:50:04 -0500

    configuration done -starting design

--- aibot/App.js
+++ aibot/App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-red-900">
+      <Text>App.js to start working on your app!</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//npx expo start
+
