22794d944f2ae27975054c71d96214b3febc88ba
isamsami <samisalah.am@gmail.com>
2024-02-02 02:28:45 -0500

    starting

--- aibot/App.js
+++ aibot/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
+import AppNavigation from './src/navigation';
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-red-900">
-      <Text>App.js to start working on your app!</Text>
-    </SafeAreaView>
+    <AppNavigation />
   );
 }
 
